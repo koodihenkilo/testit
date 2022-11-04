d2c370418f62708bef991ef91b660001172de69a
Tuukka Ylitalo <ylittuu@digitalentshelsinki.internal>
2022-11-04 11:24:29 +0200

    harjoitus-sivu

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import './Header.css'
+function Header() {
+    return (
+        
+      <div className="header">
+        <div className='wrapper'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+
+        </ul>
+        </div>
+        <button>Menu</button>
+       
+
+       </div> 
+    )
+}
+
+
+export default Header
